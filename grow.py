2da3b3d0fca9e9525654c1d16f298394852d2cb8
Kaz <114888381+Mantodkaz@users.noreply.github.com>
2024-10-31 22:10:24 +0700

    Update grow.py

--- grow.py
+++ grow.py
@@ -24,7 +24,7 @@ def refresh_bearer_token():
         print("Gagal memuat refresh_token.")
         return None
 
-    url = "htt"+"ps:"+"//secur"+"etoken.go"+"oglea"+"pis.com/v1/tok"+"en?key"+"=AIzaSyDip"+"zN0"+"VRfTPn"+"MG"+"hQ"+"5PSz"+"O27C"+"xm3D"+"ohJGY"
+    url = "https://securetoken.googleapis.com/v1/token?key=AIzaSyDipzN0VRfTPnMGhQ5PSzO27Cxm3DohJGY"
     body = {
         "grant_type": "refresh_token",
         "refresh_token": refresh_token
@@ -128,6 +128,7 @@ def run_requests():
     }
 
     while True:
+        # Selalu memperbarui token sebelum memulai iterasi
         token = refresh_bearer_token()
         if not token:
             print("Gagal memperbarui Sesi.")
